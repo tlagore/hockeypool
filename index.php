54a5526351b8da04ca5673008caa84204b8805e0
Tyrone <Tyrone@192.168.0.102>
2016-12-03 11:21:14 -0700

    More logic in pool.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 	<div class="navbar navbar-default navbar-fixed-top" id="main-navbar">
       <div class="container">
         <div class="navbar-header">
-          <a href="../" class="navbar-brand">HockeyPool!</a>
+          <a href="/hockeypool" class="navbar-brand">HockeyPool!</a>
           <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
